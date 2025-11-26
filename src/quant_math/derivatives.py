0fc807c72a88b9ae0892bbd6c48da4b19de6caa6
Karthik Iyer <karthikiyer@maximuspadpro11.localdomain>
2025-11-26 16:16:52 -0500

    day 1 completed 3 approximation formulas and plotted

---
+++ src/quant_math/derivatives.py
@@ -0,0 +1,10 @@
+def numerical_derivative(f,x,h=1e-5):
+    return (f(x + h) - f(x - h)) / (2 * h)
+
+def second_derivative(f,x,h=1e-5):
+    return (f(x+h) - (2* (f(x))) + f(x-h))/(h**2)
+
+def taylor_approx_2(f,x,x0):
+    first_der = numerical_derivative(f,x0)
+    second_der = second_derivative(f, x0)
+    return f(x0) + (first_der * (x-x0)) + (0.5 * second_der * ((x-x0)**2))
